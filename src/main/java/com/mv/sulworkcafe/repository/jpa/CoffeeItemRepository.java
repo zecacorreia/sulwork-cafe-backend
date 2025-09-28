66b9da487813aa17150bddd6fb7f4d2f6fb36c44
Italo Correia <italocdaraujo@gmail.com>
2025-09-28 15:24:12 -0300

    feat: fix CoffeeItemRepository

--- src/main/java/com/mv/sulworkcafe/repository/jpa/CoffeeItemRepository.java
+++ src/main/java/com/mv/sulworkcafe/repository/jpa/CoffeeItemRepository.java
@@ -2,5 +2,11 @@ package com.mv.sulworkcafe.repository.jpa;
 
 import com.mv.sulworkcafe.entity.CoffeeItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 
-public interface CoffeeItemRepository extends JpaRepository<CoffeeItem, Long> {}
+public interface CoffeeItemRepository extends JpaRepository<CoffeeItem, Long> {
+    @Query("SELECT i FROM CoffeeItem i JOIN FETCH i.collaborator WHERE i.event.eventDate = :date")
+    List<CoffeeItem> findByEventDateWithCollaborator(@Param("date") String date);
+}
