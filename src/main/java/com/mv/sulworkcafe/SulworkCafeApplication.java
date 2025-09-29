6f40750791a0a69fff33602d6db46a55a5780eff
Italo Correia <italocdaraujo@gmail.com>
2025-09-29 02:25:19 -0300

    feat: add component scan

--- src/main/java/com/mv/sulworkcafe/SulworkCafeApplication.java
+++ src/main/java/com/mv/sulworkcafe/SulworkCafeApplication.java
@@ -2,8 +2,10 @@ package com.mv.sulworkcafe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.mv.sulworkcafe")
 public class SulworkCafeApplication {
     public static void main(String[] args) {
         SpringApplication.run(SulworkCafeApplication.class, args);
