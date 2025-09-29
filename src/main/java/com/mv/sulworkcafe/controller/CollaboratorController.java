4e0eace0fc898235e00743f117f243c991528b71
Italo Correia <italocdaraujo@gmail.com>
2025-09-29 17:28:20 -0300

    feat: add frontend deploy url

--- src/main/java/com/mv/sulworkcafe/controller/CollaboratorController.java
+++ src/main/java/com/mv/sulworkcafe/controller/CollaboratorController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Tag(name = "Collaborators")
 @RestController
 @RequestMapping("/api/collaborators")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200, https://sulwork-cafe-frontend-production.up.railway.app")
 public class CollaboratorController {
 
     private final CollaboratorService service;
